3c47a79e725f619f8539eaf672acd5cb13373793
Hltoktay <hltoktay@gmail.com>
2019-03-03 00:58:49 +0000

    Add and style some controls

--- src/components/home.js
+++ src/components/home.js
@@ -22,6 +22,7 @@ const Home = () => {
                 </Navbar>
       
                <VideoPlayer />
+               
                    
         </div>
     )
--- src/components/video.js
+++ src/components/video.js
@@ -1,20 +1,152 @@
 import React, { Component } from 'react'
 import ReactPlayer from 'react-player'
+import { ProgressBar } from 'react-bootstrap';
 import './css/style.css';
  
 class VideoPlayer extends Component {
 
+  state = {
+    url : null,
+    playing: true,
+    controls: false,
+    volume: 0.8,
+    played: 0,
+    loaded: 0,
+    duration: 0
+  }
+
+  load = url => {
+    this.setState({
+      url,
+      played: 1,
+      loaded: 0,
+    })
+  }
+
+  playPause = () => {
+    this.setState({ playing: !this.state.playing })
+  }
+  stop = () => {
+    this.setState({ url: null, playing: false })
+  }
+
+  setVolume = e => {
+    this.setState({ volume: parseFloat(e.target.value) })
+  }
+
+  onPlay = () => {
+    console.log('onPlay')
+    this.setState({ playing: true })
+  }
+
+  onPause = () => {
+    console.log('onPause')
+    this.setState({ playing: false })
+  }
+
+  onSeekMouseDown = e => {
+    this.setState({ seeking: true })
+  }
+
+  onSeekChange = e => {
+    this.setState({ played: parseFloat(e.target.value) })
+  }
+
+  onSeekMouseUp = e => {
+    this.setState({ seeking: false })
+    this.player.seekTo(parseFloat(e.target.value))
+  }
+
+  onProgress = state => {
+    // We only want to update time slider if we are not currently seeking
+    if (!this.state.seeking) {
+      this.setState(state)
+    }
+  }
+
+  ref = player => {
+    this.player = player
+  }
+
+  addBox = e => {
+    const d = document.getElementById("content");
+    const box = document.querySelector(".box");
+    
+    if( box.style.display === "none") {
+      box.style.display = "block";
+    } else {
+      box.style.display = "none";
+    }
+   d.innerHTML += "HEllo";
+  }
+  
+
+
   render () {
+
+    
+    const { playing, controls, light, volume, played, loaded } = this.state
 
         return ( 
             <div className="main">
                 <div className="content">
                 <ReactPlayer
-                url="http://clips.vorwaerts-gmbh.de/VfE_html5.mp4"
+                ref={this.ref}
+                 url='https://www.youtube.com/embed/pWbMrx5rVBE'
+                 playing={playing}
+                 controls={controls}
+                 light={light}
+                 volume={volume}
+                 onProgress={this.onProgress}
+                 onPlay={this.onPlay}
+                 onPause={this.onPause}
+                 onSeek={e => console.log('onSeek', e)}
                 
                  />
-          
                 </div>
+                
+                <tr>
+              <th>Controls</th>
+              <td>
+                <button onClick={this.stop}>Stop</button>
+                <button onClick={this.playPause}>{playing ? 'Pause' : 'Play'}</button>
+              </td>
+            </tr>
+          <div className="seek">
+          <tr>
+              <th>Seek</th>
+              <td>
+                <p className="box" id="content"></p>
+                <input class="slider"
+                  type='range' min={0} max={1} step='any'
+                  value={played}
+                  onMouseDown={this.onSeekMouseDown}
+                  onChange={this.onSeekChange}
+                  onMouseUp={this.onSeekMouseUp}
+                  onClick={this.addBox}
+                />
+              </td>
+            </tr>
+          </div>
+           
+
+            <tr>
+              <th>Volume</th>
+              <td>
+                <input type='range' min={0} max={1} step='any' value={volume} onChange={this.setVolume} />
+              </td>
+            </tr>
+
+            <tr>
+              <th>Played</th>
+              <td><progress max={1} value={played} /></td>
+            </tr>
+
+            <tr>
+              <th>Loaded</th>
+              <td><progress max={1} value={loaded} /></td>
+            </tr>
+
 
             </div>
         )
